844e5209d11d9d3fc02c80e0384cb17477d540ef
nachtgold <zimmermann.k@gmail.com>
2016-11-05 02:56:02 +0100

    dependencies updated for elixir 1.3.4 and junitformatter for test results

--- config/test.exs
+++ config/test.exs
@@ -2,9 +2,13 @@ use Mix.Config
 
 config :logger,
   level: :info
+  #level: :debug
 
 config :servus,
 database: %{
   rootpath: "./db",
   testmode: "?mode=memory"
 }
+
+#config :junit_formatter,
+#  print_report_file: true
