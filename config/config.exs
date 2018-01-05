017f26dd0535677e3f5516c158935d27e38983c1
Christian Rölle <chris.500@t-online.de>
2018-01-05 01:53:12 +0100

    Added cardGame an Config for new TestsModule. Cardgame is a draft for a Cardgame. Implementation ongoing. First functions working and full card set.

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,9 @@ use Mix.Config
 
 # Base config
 config :servus,
-  backends: [:connect_four],
-  modules: [Player_Self, Player_Only, HiScore, Player_FB,Player_Userdata]
+  #backends: [:connect_four],
+  backends: [:testModule_1P],
+  modules: [Player_Self, Player_Only, Player_FB,Player_Userdata]
 
 # Configuration for a connect-four game
 config :servus,
@@ -14,6 +15,26 @@ connect_four: %{
   ],
   players_per_game: 2, 
   implementation: ConnectFour
+}
+
+# Configuration for a cardgame
+config :servus,
+cardGame: %{
+  adapters: [
+    tcp: 3334,
+    web: 3335
+  ],
+  players_per_game: 1, 
+  implementation: CardGame
+}
+config :servus,
+testModule_1P: %{
+  adapters: [
+    tcp: 3334,
+    web: 3335
+  ],
+  players_per_game: 1, 
+  implementation: TestModule_1P
 }
 
 config :servus,
@@ -32,7 +53,8 @@ facebook: %{
 
 config :servus,
 player_userdata: %{
-  picturepath: "./save/picture"
+  picturepath: "./save/picture",
+  profilepath: "./save/profiles"
 }
 
 import_config "#{Mix.env}.exs"
---
+++ lib/testModule/testModule.ex
@@ -0,0 +1,31 @@
+defmodule TestModule_1P do
+  use Servus.Game
+  require Logger
+
+  alias Servus.Serverutils
+
+  def init(players) do
+    Logger.info "Initializing game state machine for testModule"
+    [player1] = players
+    fsm_state = %{player1: player1, counter: 0}
+    Serverutils.send(player1.socket, ["begin_Test"], player1.name)
+    {:ok, :redo, fsm_state}
+  end
+
+
+  
+  @doc """
+  FSM is in state `p1`. Player 1 puts.
+  Outcome: p2 state
+  """
+  def redo({id, ["sendData"], data}, state) do
+    #Logger.info "Here: #{inspect data}"
+    Serverutils.send(state.player1.socket, ["reSendData"], data)
+    {:next_state, :redo, state}
+  end
+
+  def finish({ _, ["exit"], _}, state) do
+    Logger.info "Finished Game"
+  end
+
+end
